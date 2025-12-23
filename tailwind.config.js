59781f56a7ed6e56a657db183b09f4c56a0a81a7
Pranav Agarwal <agarwalpranav0711@gmail.com>
2025-12-24 01:37:07 +0530

    Initial commit: X UI clone with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '2rem',
+      },
+    },
+  },
+  plugins: [],
+}
